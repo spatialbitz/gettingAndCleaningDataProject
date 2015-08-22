1ad7f08c71219bbbe8894472ab91567d3f0cf614
Martin <spatialbits@posteo.net>
2015-08-22 20:52:48 +0200

    save status

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# gettingAndCleaningDataProject
+# Getting And Cleaning Data - Course Project Week 3
+
+
+
+
+## License:
+
+========
+
+Use of this dataset in publications must be acknowledged by referencing the following publication [1] 
+
+
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+
---
+++ run_analysis.R
@@ -0,0 +1,92 @@
+ytest <- read.table("./rawdata/test/y_test.txt")
+ytrain <- read.table("./rawdata/train/y_train.txt")
+xtest <- read.table("./rawdata/test/X_test.txt")
+xtrain <- read.table("./rawdata/train/X_train.txt")
+trainsubject <- read.table("./rawdata/train/subject_train.txt")
+testsubject <- read.table("./rawdata/test/subject_test.txt")
+
+ytest <- rename(ytest, Activity = V1)
+testsubject <- rename(testsubject, ProbandID = V1)
+testcases <- (cbind(xtest,ytest, testsubject))
+
+ytrain <- rename(ytrain, Activity = V1)
+trainsubject <- rename(trainsubject, ProbandID = V1)
+traincases <- (cbind(xtrain,ytrain, trainsubject))
+
+allcases <- rbind(testcases,traincases)
+
+features <- read.table("./rawdata/features.txt")
+features$meanT <- ifelse(grepl("mean", features$V2), T, F)
+features$stdT <- ifelse(grepl("std", features$V2), T, F)
+
+
+
+labels <- features$V2
+labels2 <-as.factor(c("Activity","ProbandID"))
+labels <- c(as.character(labels), as.character(labels2))
+names(allcases) <- labels
+
+allMeanStd <- select(allcases, as.integer(rownames(filter(features, meanT == T | stdT == T))))
+allMeanStd <- cbind(allMeanStd, allcases$Activity, allcases$ProbandID)
+
+
+features <- filter(features, meanT == T | stdT == T)
+labels <- features$V2
+labels2 <- as.factor(c("Activity","ProbandID"))
+labels <- c(as.character(labels), as.character(labels2))
+
+names(allMeanStd) <- labels
+
+activityLabels <- read.table("./rawdata/activity_labels.txt")
+allMeanStd <- mutate(allMeanStd, Activity = activityLabels$V2[Activity])
+
+write.table(allMeanStd, file="tidydata/step4_allMeanStd.txt")
+
+
+allAv <- allMeanStd
+allAvMelt <- melt(allAv, id=c("ProbandID","Activity"))
+allAvMelt <- mutate(allAvMelt, value = as.numeric(value))
+
+walking_all <- filter(allAvMelt, Activity == "WALKING")
+walking_upstairs_all <- filter(allAvMelt, Activity == "WALKING_UPSTAIRS")
+walking_downstairs_all <- filter(allAvMelt, Activity == "WALKING_DOWNSTAIRS")
+sitting_all <- filter(allAvMelt, Activity == "SITTING")
+standing_all <- filter(allAvMelt, Activity == "STANDING")
+laying_all <- filter(allAvMelt, Activity == "LAYING")
+
+walking_all <- dcast(walking_all, ProbandID ~ variable, mean)
+walking_all$Activity = "WALKING_ALL"
+
+walking_upstairs_all <- dcast(walking_upstairs_all, ProbandID ~ variable, mean)
+walking_upstairs_all$Activity = "WALKING_UPSTAIRS"
+
+walking_downstairs_all <- dcast(walking_downstairs_all, ProbandID ~ variable, mean)
+walking_downstairs_all$Activity = "WALKING_DOWNSTAIRS"
+
+sitting_all <- dcast(sitting_all, ProbandID ~ variable, mean)
+sitting_all$Activity = "SITTING"
+
+standing_all <- dcast(standing_all, ProbandID ~ variable, mean)
+standing_all$Activity = "STANDING"
+
+laying_all <- dcast(laying_all, ProbandID ~ variable, mean)
+laying_all$Activity = "LAYING"
+
+average_all <- rbind(walking_all, walking_downstairs_all, walking_upstairs_all, sitting_all, standing_all, laying_all)
+
+write.table(average_all, file="tidydata/step5_average_all.txt")
+
+attr(average_all, "source") <- "http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones"
+attr(average_all, "license") <- "Use of this dataset in publications must be acknowledged by referencing the following publication: Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012"
+attr(average_all, "disclaimer") <- "This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited."
+
+codeBookStub <-  capture.output(str(average_all))
+write(codeBookStub, file = "CodeBook.md")
+
+
+
+# notes
+libraryies(!) 
+CodeBook
+README
+
